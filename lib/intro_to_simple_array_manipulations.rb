e56b817bce1c2da983147a3e20d46bd9674ecf25
Josh Beglan <josh_beglan@yahoo.com>
2020-03-03 01:57:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,3 +21,27 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
+
+def using_concat(array2, array2) 
+  array1 + array2
+end
+
+def using_insert(array, element)
+  array.insert(element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
